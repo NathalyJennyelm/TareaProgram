4a6fd73466e7417f4c443f5657269e70a4821743
NathalyJennyelm <nathalyobando1@gmail.com>
2024-11-08 14:20:21 -0600

    Se crea la clase producto con su constructor

---
+++ Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario
+{
+    public class Producto
+    {
+        // Propiedades para almacenar el nombre y precio del producto
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+        // Constructor que inicializa el nombre y el precio del producto
+        public Producto(string nombre, decimal precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
+    }
+}
