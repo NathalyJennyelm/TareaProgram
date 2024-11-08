05c2e90f1319fc8e3daa8b3994b0dbbe971ba494
NathalyJennyelm <nathalyobando1@gmail.com>
2024-11-08 14:28:21 -0600

    Se crea la clase inventario, y dentro de ella se crea una lista para almacenar los productos y un metodo para agregar producto

---
+++ Inventario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario
+{
+    public class Inventario
+    {
+        // Lista para almacenar los productos
+        private List<Producto> productos = new List<Producto>();
+
+        // Método para agregar un producto a la lista
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+    }
+}
